4d8b4e24265758521dbe0228bb966a6019e0144f
benjamingarciap <bengarc@gmail.com>
2019-08-11 20:45:10 +0200

    initial commit to heroku

--- client/src/components/MovieDetails.js
+++ client/src/components/MovieDetails.js
@@ -23,7 +23,7 @@ const MovieDetails = (props) => {
 
 
   useEffect(()=>{
-    axios.get(`http://localhost:5000/api/movies/${props.match.params.id}`)
+    axios.get(`${process.env.REACT_APP_API_URL}/api/movies/${props.match.params.id}`)
     .then( responseFromApi =>{
 
       setData({ movie:responseFromApi.data })
@@ -34,7 +34,7 @@ const MovieDetails = (props) => {
 
   const deleteMovie = () => {
         const { params } = props.match
-        axios.delete(`http://localhost:5000/api/movies/${params.id}`)
+        axios.delete(`${process.env.REACT_APP_API_URL}/api/movies/${params.id}`)
         .then( responseFromApi=>{
           props.history.push('/')
         })
